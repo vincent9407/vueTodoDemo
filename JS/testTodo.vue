59622bf423c9ac8a56a15e5821f4de3e212ab397
LangDonHJJ <quattrohym@163.com>
2016-10-19 13:14:26 +0800

    Complete version 1.0

---
+++ JS/testTodo.vue
@@ -0,0 +1,22 @@
+new Vue({
+    el : '#app',
+    data : {
+        newTodo : '',
+        todos : [
+            { text : 'Add Some Todos' }
+        ]
+    },
+    methods : {
+        addTodo : function(){
+            var text = this.newTodo.trim()
+            if(text){
+                this.todos.push({ text : text })
+                this.newTodo = ''
+            }
+        },
+        removeTodo: function (index) {
+            console.log(index);
+            this.todos.splice(index, 1)
+        }
+    }
+});
